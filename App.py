739e097048a5d72a227def107caa9b2c2d139ce4
Luis Eduardo Sánchez <lsgm16308@gmail.com>
2021-03-29 22:05:54 -0600

    App

--- App.py
+++ App.py
@@ -1,15 +1,14 @@
 """
-Interfaz gráfica
+Interfaz gráfica para control de interferómetro
 
 Universidad Autonoma de Coahuila
 Facultad de Ciencias Físico Matemáticas 
 
 dom 21 mar 2021 12:11:39 CST 
 """
-import sys
+from PyQt5 import QtWidgets, QtCore
 import serial, time
-from PyQt5 import QtWidgets
-from PyQt5.QtCore import *
+import sys
 
 class MyApp(QtWidgets.QWidget):
     
@@ -17,48 +16,87 @@ class MyApp(QtWidgets.QWidget):
      
 		QtWidgets.QWidget.__init__(self)
 
-		layout = QtWidgets.QGridLayout()
-		self.setLayout(layout)
+		#menubar = self.menuBar() 
+		#file_menu = menubar.addMenu('&File') 
+		#help_menu = menubar.addMenu('&Help') 
 
-		menubar = QtWidgets.QMenuBar()
-		layout.addWidget(menubar, 0, 0)
-		actionFile = menubar.addMenu("File")
-		actionFile.addAction("New")
-		actionFile.addAction("Open")
-		actionFile.addAction("Save")
-		actionFile.addSeparator()
-		actionFile.addAction("Quit")
-		menubar.addMenu("Edit")
-		menubar.addMenu("View")
-		menubar.addMenu("Help")
+		self.edit = QtWidgets.QLineEdit(">>> \n >>>")
+		self.edit.setClearButtonEnabled(False)       		
 
-		Encender = QtWidgets.QPushButton('Encender', self)
-		Encender.clicked.connect(self.ON)
-		Encender.move(200, 200)
-		Encender.show()
+		layout = QtWidgets.QVBoxLayout()
+		layout.addWidget(self.edit)
 
-		Apagar = QtWidgets.QPushButton('Apagar', self)
-		Apagar.clicked.connect(self.OFF)
-		Apagar.move(300, 200)
-		Apagar.show()
+		First_Layout = QtWidgets.QVBoxLayout()
+		Second_Layout = QtWidgets.QHBoxLayout()
+
+		self.setLayout(First_Layout)
+		First_Layout.addLayout(layout)								#Ubicamos el Layout en la Widget
+		First_Layout.addLayout(Second_Layout)
+
+		self.Select_Vel = QtWidgets.QLabel("Selector de velocidad")
+		self.Select_Vel.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignCenter)
+		First_Layout.addWidget(self.Select_Vel)
+
+		self.Velocity = QtWidgets.QSpinBox()							#Spin Box para la velocidad
+		First_Layout.addWidget(self.Velocity)							#Agregamos Spin Box a layout
+        
+	        #Ajustes del Spin Box (Velocity)
+		self.Velocity.setMaximum(20000)
+		self.Velocity.setMinimum(4000)
+		self.Velocity.setSingleStep(100)
+		self.Velocity.setPrefix("Vel = ")
+		self.Velocity.setSuffix(" steps")
+		self.Velocity.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignCenter)
+
+		self.Vel = QtWidgets.QPushButton("Seleccionar")
+		First_Layout.addWidget(self.Vel) 	
+		self.Vel.clicked.connect(self.Select)
+
+		self.Conectar = QtWidgets.QPushButton('Conectar')
+		Second_Layout.addWidget(self.Conectar)
+		self.Conectar.clicked.connect(self.Connect)
+		
+		self.Encender = QtWidgets.QPushButton('Encender')
+		Second_Layout.addWidget(self.Encender)
+		self.Encender.clicked.connect(self.ON)
+
+		self.Apagar = QtWidgets.QPushButton('Apagar')
+		Second_Layout.addWidget(self.Apagar)
+		self.Apagar.clicked.connect(self.OFF)
+
+	def To_Select_Velocity(self):
+
+		"""
+		Crea layout donde se encontrará el selector de velocidad.
+		"""
+
+		pass
+
+	def Connect(self):
+		
+		self.arduino = serial.Serial("/dev/ttyACM0", baudrate=9600, timeout=1.0)
+
+	def Select(self):
+
+		self.arduino.write(b'V')
+		print("Velocidad Seleccionada")
+		print(self.Velocity.value())	
+		self.arduino.write(bytes(str(self.Velocity.value()), "utf-8"))
 
 	def ON(self):
 
-		arduino.write(b'S')
+		self.arduino.write(b'S')
 
 	def OFF(self):
 
-		arduino.write(b'N')
+		self.arduino.write(b'N')
 
 if __name__ == '__main__':					
 
 	app = QtWidgets.QApplication(sys.argv)
 
-	#arduino = serial.Serial("/dev/ttyACM0", baudrate=9600, timeout=1.0)
-
 	window = MyApp()
-
-	window.resize(600, 600)
+	#window.resize(600, 600)
 	#window.move(300, 300)
 	window.setWindowTitle("Control interferómetro")
 
---
+++ Pruebas/Prueba/Prueba.ino
@@ -0,0 +1,85 @@
+#include<LCD.h>
+#include <Wire.h>  // Lo trae Arduino IDE
+#include <LiquidCrystal_I2C.h> // Incluimos la libreria del LCD
+
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Seteamos la direccion I2C
+
+char opcion1 = '0';
+int data;
+
+void setup() {
+
+  lcd.begin(20,4); 
+  Serial.begin(9600);
+  Serial.setTimeout(1000);
+  pinMode(13, OUTPUT);
+ 
+  lcd.setCursor(3,0); //Empiezo en la posicion 1 (caracter) sobre la linea 0
+  lcd.print("Bienvenido");
+  lcd.setCursor(1,1);
+  lcd.print("Esto es una prueba");  
+
+}
+
+int Suma(int num){
+  return num + num;
+}
+
+void loop(){
+  
+  if(Serial.available()>0){
+    
+    opcion1=Serial.read();
+    Serial.flush();
+    
+    switch(opcion1){
+      
+      case 'S':
+      
+            digitalWrite(13, HIGH);
+      
+        break;
+        
+      case 'N':
+      
+           digitalWrite(13, LOW);
+           
+        break;
+
+      case 'V':
+
+            lcd.setCursor(1,3);
+            lcd.print("Velocidad: "); 
+            
+            delay(2000);
+            String bufferString = "";
+
+            while (Serial.available() > 0) {
+                bufferString += (char)Serial.read();
+            }
+            
+            //Se transforma el buffer a un número entero
+            int num = bufferString.toInt();
+            //Se imprime el número que se recibe
+            //Serial.print("Numero recibido: ");
+            Serial.println(num);
+
+            Serial.println(Suma(num));
+            
+            delay(2000);
+            lcd.setCursor(13,3);
+            lcd.print(num);         
+
+            break;
+
+      default:
+
+           Serial.println("No se selecciono nada");
+
+           break;
+        
+    }//Fin Switch
+    
+  }
+  
+}
