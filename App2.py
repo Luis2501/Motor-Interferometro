4ef66f9975bf5d83dc18964af290bf31c270feb4
Luis Eduardo Sánchez <lsgm16308@gmail.com>
2021-04-02 22:00:24 -0600

    Puertos serie

--- App2.py
+++ App2.py
@@ -6,9 +6,9 @@ Facultad de Ciencias Físico Matemáticas
 
 dom 21 mar 2021 12:11:39 CST 
 """
-from datetime import datetime
-from Settings import Ui_MainWindow2
 from PyQt5 import QtWidgets, QtCore, uic
+from Settings import Ui_MainWindow2
+from datetime import datetime
 import serial, time
 import sys
 
@@ -17,25 +17,31 @@ UI_CLASS, UI_BASE_CLASS = uic.loadUiType("Main.ui")
 class MyApp(UI_CLASS, UI_BASE_CLASS):
 
 	def __init__(self):
+		
+		"""
+		Parámetros:
+	
+		Arduino_Condition: Indica el estado de conexión con arduino
+		"""
 
 		UI_BASE_CLASS.__init__(self)
 
 		self.setupUi(self)
 	
-		#Ventana de ajustes
+		#Ventana de ajustes-------------------------------
 		
 		self.Window_Settings_Port = QtWidgets.QMainWindow()
 		self.Settings = Ui_MainWindow2()
 		self.Settings.setupUi(self.Window_Settings_Port)
 		
-		#--------------
+		#-------------------------------------------------
 
-		self.Arduino_Condition = False
+		self.Arduino_Condition = False	
 
-		self.Connect_Arduino.clicked.connect(self.Connect)
-		self.Disconnect_Arduino.clicked.connect(self.Disconnect)
-
-		self.Select_Velocity.clicked.connect(self.Select)
+		self.Connect_Arduino.clicked.connect(self.Connect)					#Botón "Conectar"
+		self.Disconnect_Arduino.clicked.connect(self.Disconnect)				#Botón "Desconectar"
+	
+		self.Select_Velocity.clicked.connect(self.Select)					#Botón "Seleccionar" velocidad
 
 		self.Clean_Display.clicked.connect(self.Display.clear)
 		self.Display.append(">>> Interferómetro (" + str(datetime.now()) + ") \n")
@@ -49,15 +55,28 @@ class MyApp(UI_CLASS, UI_BASE_CLASS):
 		self.Settings.Button_Windows.toggled.connect(self.Show_List)
 		self.Settings.Button_Linux.toggled.connect(self.Show_List)
 
-	def Show_List(self, System):
+		self.Settings.Select_Port.clicked.connect(self.Select_Port_Serial)
+
+	def Select_Port_Serial(self):
+
+		self.Port_Serial = (self.Settings.Port_list.currentItem()).text()
+
+		self.Display.append(">>> Puerto seleccionado")
+		self.Display.append(">>> Puerto: " + self.Port_Serial + "\n")
+
+	def Show_List(self):
 
 		if self.Settings.Button_Windows.isChecked():
+
+			self.Settings.Port_list.clear()
 
 			for k in range(len(self.Windows)):
 
 				self.Settings.Port_list.insertItem(k, self.Windows[k]) 
 
 		elif self.Settings.Button_Linux.isChecked():
+
+			self.Settings.Port_list.clear()	
 
 			for k in range(len(self.Linux)):
 
@@ -73,11 +92,17 @@ class MyApp(UI_CLASS, UI_BASE_CLASS):
 		Realiza conexión con arduino.
 		"""
 
-		self.Arduino_Condition = True
-		
-		self.arduino = serial.Serial("/dev/ttyACM0", baudrate=9600, timeout=1.0)
+		try:
+
+			self.arduino = serial.Serial(self.Port_Serial, baudrate=9600, timeout=1.0)
 	
-		self.Display.append(">>> Arduino conectado \n")
+			self.Arduino_Condition = True
+
+			self.Display.append(">>> Arduino conectado \n")
+		
+		except:
+
+			self.Display.append(">>> Fallo al conectar con arduino \n")
 
 	def Disconnect(self):
 
@@ -85,13 +110,24 @@ class MyApp(UI_CLASS, UI_BASE_CLASS):
 		Termina la conexión con arduino		
 		"""
 
-		self.Arduino_Condition = False
+		try: 
 
-		self.arduino.close()
+			self.arduino.close()
+			self.Arduino_Condition = False	
 
-		self.Display.append(">>> Arduino desconectado \n")
+		except:
+
+			pass
+
+		finally:		
+
+			self.Display.append(">>> Arduino desconectado \n")
 
 	def Select(self):
+
+		"""
+		Selecciona la velocidad del motor
+		"""
 
 		if self.Arduino_Condition:
 
